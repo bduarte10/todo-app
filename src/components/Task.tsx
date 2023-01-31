ca81fd860a34f46de9f30fe41a341018a5948d85
Bruno Duarte <duartebruno2208@gmail.com>
2023-01-31 13:07:22 -0300

    feature: add background color on checkbox hover

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -18,9 +18,9 @@ const Task = ({
     <div className="max-w-[736px] w-full bg-gray-500 m-auto border border-gray-400 rounded-lg flex items-center  px-4 py-4 mb-3">
       <button
         onClick={handleIsComplete}
-        className="w-4 h-4 border border-blue rounded-full flex items-center justify-center mr-4">
+        className="w-4 h-4 border border-blue rounded-full flex items-center justify-center hover:bg-blue-dark/20 mr-4">
         {isCompleted ? (
-          <span className="w-4 h-4 bg-purple-dark rounded-full  flex items-center justify-center">
+          <span className="w-4 h-4 bg-purple-dark hover:bg-purple rounded-full  flex items-center justify-center">
             <AiOutlineCheck
               className="text-gray-100"
               size={8}
